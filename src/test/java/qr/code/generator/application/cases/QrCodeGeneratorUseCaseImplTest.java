a8ec50b926ad7d333657085dd9adaefe1aa9471b
luiz <luizhesafa21@gmail.com>
2025-07-13 19:08:35 -0300

    Create jacococ in project

--- src/test/java/qr/code/generator/application/cases/QrCodeGeneratorUseCaseImplTest.java
+++ src/test/java/qr/code/generator/application/cases/QrCodeGeneratorUseCaseImplTest.java
@@ -1,6 +1,5 @@
 package qr.code.generator.application.cases;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
